1720c824cd66454099788f072b829aa35b72c29b
Alex K. <git config --global user.email>
2025-08-25 21:36:06 +0300

    7.1. For, 1-8 exc.

--- 7.1 For.py
+++ 7.1 For.py
@@ -1,1 +1,81 @@
-# Тема урока: цикл for 
+# Тема урока: цикл for
+from difflib import Match
+
+#1 Python is awesome
+# for i in range(10):
+#     print('Python is awesome!')
+
+#2 Последовательность символов
+# for i in range(6):
+#     print('AAA')
+# for i in range(5):
+#     print('BBBB')
+# print('E')
+# for i in range(9):
+#     print('TTTTT')
+# print('G')
+
+#3 Повторяй за мной 1
+# a = input()
+# b = int(input())
+#
+# for i in range(b):
+#     print(a)
+
+#4 Звёздный прямоугольник
+# n = int(input())
+#
+# for i in range(n):
+#     print('*' * 19)
+
+# Теория
+# for i in range(10):
+#     print(i, '-- Привет')
+#  ==
+# 0 -- Привет
+# 1 -- Привет
+# 2 -- Привет
+# 3 -- Привет
+# 4 -- Привет
+# 5 -- Привет
+# 6 -- Привет
+# 7 -- Привет
+# 8 -- Привет
+# 9 -- Привет
+#
+# for i in range(10):
+#     print(i + 1, '-- Привет')
+#  ==
+# 1 -- Привет
+# 2 -- Привет
+# 3 -- Привет
+# 4 -- Привет
+# 5 -- Привет
+# 6 -- Привет
+# 7 -- Привет
+# 8 -- Привет
+# 9 -- Привет
+# 10 -- Привет
+
+#5 Повторяй за мной 2
+# a = input()
+#
+# for i in range(10):
+#     print(i, a)
+
+#6 Квадрат числа
+# n = int(input())
+#
+# for i in range(n + 1):
+#     print('Квадрат числа', i, 'равен', n)
+
+#7 Звёздный треугольник
+# n = int(input())
+#
+# for i in range(n, 0, -1):
+#     print('*' * i)
+
+#8 Популяция
+# ---
+
+# End
