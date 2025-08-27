c93bd19cec6f8972e65cd9e48a82b238458d5aae
Alex K. <alexkuzmma@yandex.ru>
2025-08-27 14:22:45 +0300

    7.2 For_range_3++

--- 7.2 For_range.py
+++ 7.2 For_range.py
@@ -24,5 +24,17 @@
 m = int(input())
 n = int(input())
 
-for i in range(m, n, -2):
-    print(i)
+if m % 2 == 0:
+    if m - 1 == n and m % 2 == 0:
+        print(n)
+    if m - 1 == n and n % 2 == 0:
+        print(m)
+if m % 2 == 0 and m - 1 != n:
+    for i in range(m - 1, n, -2):
+        print(i)
+if m % 2 != 0 and n % 2 != 0:
+    for i in range(m, n - 1, -2):
+        print(i)
+if m % 2 != 0 and n % 2 == 0:
+    for i in range(m, n, -2):
+        print(i)
