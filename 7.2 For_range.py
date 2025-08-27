23d5423a5b16dba896073a1e630196ea382b1997
Alex K. <alexkuzmma@yandex.ru>
2025-08-27 14:42:47 +0300

    7.2 For_range_5++

--- 7.2 For_range.py
+++ 7.2 For_range.py
@@ -21,20 +21,38 @@
 #     print(m)
 
 #3 Последовательность чисел 3
-m = int(input())
-n = int(input())
+# m = int(input())
+# n = int(input())
+#
+# if m % 2 == 0:
+#     if m - 1 == n and m % 2 == 0:
+#         print(n)
+#     if m - 1 == n and n % 2 == 0:
+#         print(m)
+# if m % 2 == 0 and m - 1 != n:
+#     for i in range(m - 1, n, -2):
+#         print(i)
+# if m % 2 != 0 and n % 2 != 0:
+#     for i in range(m, n - 1, -2):
+#         print(i)
+# if m % 2 != 0 and n % 2 == 0:
+#     for i in range(m, n, -2):
+#         print(i)
 
-if m % 2 == 0:
-    if m - 1 == n and m % 2 == 0:
-        print(n)
-    if m - 1 == n and n % 2 == 0:
-        print(m)
-if m % 2 == 0 and m - 1 != n:
-    for i in range(m - 1, n, -2):
-        print(i)
-if m % 2 != 0 and n % 2 != 0:
-    for i in range(m, n - 1, -2):
-        print(i)
-if m % 2 != 0 and n % 2 == 0:
-    for i in range(m, n, -2):
-        print(i)
+#4 Последовательность чисел 4
+# m = int(input())
+# n = int(input())
+#
+# for i in range(m, n):
+#     if i % 3 == 0 or i % 5 == 0 or i % 15 == 0:
+#         print()
+#     if i % 10 == 9:
+#         print(9)
+#     if i % 17 == 0:
+#         print(17)
+
+#5 Таблица умножения
+a = int(input())
+
+for i in range(10):
+    print(a, 'x', i + 1, '=', a * i + a)
