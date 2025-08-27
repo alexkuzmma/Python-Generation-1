adc46bcecc3d5a8d17893672c7204fb9ed67f23b
Alex K. <alexkuzmma@yandex.ru>
2025-08-27 14:04:42 +0300

    7.2 For_range_2

--- 7.2 For_range.py
+++ 7.2 For_range.py
@@ -1,10 +1,21 @@
 # Тема урока: цикл for
 
 #1. Последовательность чисел 1
+# m = int(input())
+# n = int(input())
+#
+# for i in range(m, n + 1):
+#     print(i)
+
+#2 Последовательность чисел 2
 m = int(input())
 n = int(input())
 
-for i in range(m, n + 1):
-    print(i)
-
-#2.
+if m < n:
+    for i in range(m, n + 1):
+        print(i)
+if m > n:
+    for i in range(m, n - 1, -1):
+        print(i)
+if m == n:
+    print(m)
