e1d15ae2d108f3af4fe5b4cbb1a5319cbe89f23e
Alex K. <alexkuzmma@yandex.ru>
2025-09-11 19:35:08 +0300

    Informatics lesson 1

--- Informatics.py
+++ Informatics.py
@@ -10,9 +10,14 @@
 # print(a // 60, a % 60)
 
 # 1.3
-a, b = int(input()), int(input())
+# a, b = int(input()), int(input())
+#
+# if a == b:
+#     print('YES')
+# else:
+#     print('NO')
 
-if a == b:
-    print('YES')
-else:
-    print('NO')
+# 1.4
+a, b, c, d, e, k = int(input()), int(input()), int(input()), int(input()), int(input()), int(input())
+
+print(min(a, b, c, d, e, k))
